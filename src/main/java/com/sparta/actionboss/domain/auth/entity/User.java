bd4d5025ea28467a5ff5767cfd709a753b43b850
s2cocos2 <yewon0309@naver.com>
2023-08-05 15:39:29 +0900

    feat : create entity

--- src/main/java/com/sparta/actionboss/domain/auth/entity/User.java
+++ src/main/java/com/sparta/actionboss/domain/auth/entity/User.java
@@ -1,2 +1,38 @@
-package com.sparta.actionboss.domain.auth.entity;public class User {
+package com.sparta.actionboss.domain.auth.entity;
+
+import com.sparta.actionboss.domain.post.entity.Post;
+import com.sparta.actionboss.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> postList = new ArrayList<>();
 }
