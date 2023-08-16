c4a28c339b258a0f976b5a6a9ac88625b620158d
Yejin <dpwls8364@naver.com>
2023-08-16 20:03:41 +0900

    feat: add response postId(create post)

--- src/main/java/com/sparta/actionboss/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/sparta/actionboss/domain/post/dto/PostResponseDto.java
@@ -1,30 +1,36 @@
 package com.sparta.actionboss.domain.post.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sparta.actionboss.domain.post.entity.Post;
 import com.sparta.actionboss.global.security.UserDetailsImpl;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
 @Getter
 public class PostResponseDto {
     private final Long postId;
-    private final String title;
-    private final String content;
-    private final List<String> imageUrlList;
-    private final String address;
-    private final Double latitude;
-    private final Double longitude;
+    private String title;
+    private String content;
+    private List<String> imageUrlList;
+    private String address;
+    private Double latitude;
+    private Double longitude;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private final LocalDateTime createdAt;
-    private final String nickname;
-    private final int doneCount;
-    private final boolean done;
-    private final boolean owner;
+    private LocalDateTime createdAt;
+    private String nickname;
+    private Integer doneCount;
+    private Boolean done;
+    private Boolean owner;
+    private Boolean agree;
+    private Integer agreeCount;
 
-    public PostResponseDto(Post post, List<String> imageURLs, boolean done, boolean owner) {
+    public PostResponseDto(Post post, List<String> imageURLs, boolean done, boolean owner, boolean agree) {
         this.postId = post.getPostId();
         this.title = post.getTitle();
         this.content = post.getContent();
@@ -35,7 +41,13 @@ public class PostResponseDto {
         this.createdAt = post.getCreatedAt();
         this.nickname = post.getUser().getNickname();
         this.doneCount = post.getPostDoneList().size();
+        this.agree = agree;
+        this.agreeCount = post.getPostAgreeList().size();
         this.done = done;
         this.owner = owner;
+    }
+
+    public PostResponseDto(Long postId) {
+        this.postId = postId;
     }
 }
