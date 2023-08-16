bbe8fe41e5841adf5a0d82e9b160f2ac4ff7db37
soon91 <vbc1124@gmail.com>
2023-08-16 14:54:32 +0900

    feat: Filter by coordinate range

--- src/main/java/com/sparta/actionboss/domain/post/service/PostGetService.java
+++ src/main/java/com/sparta/actionboss/domain/post/service/PostGetService.java
@@ -8,10 +8,7 @@ import com.sparta.actionboss.domain.post.entity.Post;
 import com.sparta.actionboss.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,18 +25,15 @@ public class PostGetService {
 
     private final PostRepository postRepository;
 
-    public PostListAndTotalPageResponseDto getPostList(Integer page, Integer limit, String sortBy, boolean done) {
+    public PostListAndTotalPageResponseDto getPostList(Integer page, Integer limit, String sortBy, boolean done,
+             Double northLatitude, Double eastLongitude, Double southLatitude, Double westLongitude) {
         Sort.Direction direction = Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy, "createdAt");
 
         Pageable pageable = PageRequest.of(page, limit, sort);
         Page<Post> post;
 
-        if (done) {
-            post = postRepository.findByDoneIsTrue(pageable);
-        } else {
-            post = postRepository.findByDoneIsFalse(pageable);
-        }
+        post = postRepository.findFilteredPosts(done, northLatitude, eastLongitude, southLatitude, westLongitude, pageable);
 
         List<PostListResponseDto> postListResponseDtos = post.stream()
                 .map(a -> {
@@ -71,7 +65,8 @@ public class PostGetService {
         return new PostModalResponseDto(findPost, imageUrl);
     }
 
-    public List<MapListResponseDto> getMapList(boolean done) {
+    public List<MapListResponseDto> getMapList(boolean done,
+             Double northLatitude, Double eastLongitude, Double southLatitude, Double westLongitude) {
         List<Post> post;
 
         if (done) {
@@ -79,8 +74,12 @@ public class PostGetService {
         } else {
             post = postRepository.findByDoneIsFalse();
         }
+        // TODO : 로직 내 필터링 추가와 쿼리문에서 필터해서 가져오는 방법 중 고민 필요
+        List<Post> filteredPosts = post.stream()
+                .filter(p -> isWithinCoordinates(p, northLatitude, eastLongitude, southLatitude, westLongitude))
+                .collect(Collectors.toList());
 
-        List<MapListResponseDto> mapListResponseDto = post.stream()
+        List<MapListResponseDto> mapListResponseDto = filteredPosts.stream()
                 .map(a -> new MapListResponseDto(
                         a.getPostId(),
                         a.getLatitude(),
@@ -90,6 +89,13 @@ public class PostGetService {
                 .collect(Collectors.toList());
 
         return mapListResponseDto;
+    }
+
+    private boolean isWithinCoordinates(Post post, Double north, Double east, Double south, Double west) {
+        Double latitude = post.getLatitude();
+        Double longitude = post.getLongitude();
+
+        return latitude >= south && latitude <= north && longitude >= west && longitude <= east;
     }
 
 }
