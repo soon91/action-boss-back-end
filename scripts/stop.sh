016f9a61e96b2b82148808a083841a849abf3736
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-08-30 22:45:30 +0900

    docs: modify stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
+PROJECT_ROOT="/home/ubuntu/test"
 JAR_FILE="$PROJECT_ROOT/build/libs/Action-Boss-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
