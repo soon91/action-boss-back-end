34bfdb343545b90edd2c1fac4dc51755f283c4bc
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-08-30 23:21:22 +0900

    fix: modify start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/test"
+PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/build/libs/Action-Boss-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
